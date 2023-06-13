e076503a8d4cb358ff346062ee9edb4999660c97
korealu <643949593@qq.com>
2023-06-13 10:42:59 +0800

    fix(registerpinaia): registerPinaia:全局注册pinia
    registerPinaia:全局注册pinia

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 
 import 'normalize.css'
+import registerPinia from './store'
 
 const app = createApp(App)
+registerPinia(app)
 app.use(router)
 app.mount('#app')
