e677eb86fe246afafcc675821246424d990eb80a
korealu <643949593@qq.com>
2023-08-25 11:50:32 +0800

    fix(korealu): 骨架屏代码优化
    骨架屏代码优化

--- src/views/front-home/index.vue
+++ src/views/front-home/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>这是首页</div>
+  <HySkeleton v-model:loading="loading" />
+  <div v-if="!loading">这是首页</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HySkeleton from '@/base-ui/skeleton'
+import { ref } from 'vue'
+
+const loading = ref(true)
+</script>
 
 <style scoped></style>
